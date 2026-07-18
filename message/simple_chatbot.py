a038b5b8b5bf985fde3a48cf9b012e76b333d534
asifikbal09 <asif.ikbal662@gmail.com>
2026-07-19 00:52:37 +0600

    [UPDATE] Refactor chatbot logic to utilize chat history for context in model invocation

--- message/simple_chatbot.py
+++ message/simple_chatbot.py
@@ -1,18 +1,26 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from langchain.messages import HumanMessage, AIMessage, SystemMessage
 
 load_dotenv()
 
 model = ChatGroq(
     model="llama-3.3-70b-versatile"
 )
+
+chat_history = [SystemMessage(content="You are a helpful assistant. Always answer in a 2-3 sentence format. If you don't know the answer, say 'I don't know'.")]
+
 while True:
     user_input = input("You :")
+    # chat_history.append({"role": "user", "content": user_input})
+    chat_history.append(HumanMessage(content=user_input))
     
     if user_input.lower() == "exit":
         break
     
-    result = model.invoke(user_input)
+    result = model.invoke(chat_history)
+    # chat_history.append({"role": "assistant", "content": result.content})
+    chat_history.append(AIMessage(content=result.content))
     print("Bot :", result.content)
     
     
